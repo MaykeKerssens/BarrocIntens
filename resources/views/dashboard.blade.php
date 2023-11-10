2fe387e6cae8be924c88b9a1d87923c96651c31e
MaykeKerssens <93595867+MaykeKerssens@users.noreply.github.com>
2023-11-10 15:59:00 +0100

    App layout toegevoegd op meeste pagina's

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,15 @@
 <x-app-layout>
-    {{-- <x-slot name="pageHeaderText">
+    <x-slot name="pageHeaderText">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dit is een page header') }}
+            {{ __('Dashboard') }}
         </h2>
-    </x-slot> --}}
+    </x-slot>
 
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="bg-white  overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 ">
                     {{ __("You're logged in!") }}
                 </div>
             </div>
