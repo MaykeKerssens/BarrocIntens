a0e23dd1628e86e8cd496ccf34e5d653353da9b8
MaykeKerssens <93595867+MaykeKerssens@users.noreply.github.com>
2023-11-10 15:09:54 +0100

    Installed Breeze

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,19 @@
+<nav>
+    <!-- Name + Slogan -->
+    <div class="flex items-center p-5 text-4xl">
+        <a href="{{ route('dashboard') }}" class="flex gap-x-1 font-bigShouldersDisplay">
+            <h1 class="">Barroc</h1>
+            <h1 class="font-bigShouldersDisplayBold">Intens</h1>
+            <h2>- vl</h2>
+            <h2 class="text-[#ffd700] font-bigShouldersDisplayBlack">/</h2>
+            <h2>mmend lekkere koffie</h2>
+        </a>
+    </div>
+</nav>
+
+
+
+
+
+
+
