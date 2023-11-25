a1ac73ff8bd123ec30dd02c70f125dce25042a7a
Joost <93526867+Joost56754@users.noreply.github.com>
2023-11-25 18:05:00 +0100

    Index styling toegevoegd

--- resources/views/sourcing/index.blade.php
+++ resources/views/sourcing/index.blade.php
@@ -5,47 +5,77 @@
         </h2>
     </x-slot>
 
-    <!-- Add a button to navigate to the create page -->
+    <style>
+        /* Inline CSS-stijlen voor de tabel */
+        .table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+        }
+
+        .table th,
+        .table td {
+            padding: 8px;
+            border: 1px solid #ddd;
+        }
+
+        .table th {
+            background-color: #f2f2f2;
+        }
+
+        .table img {
+            max-width: 100px;
+            max-height: 100px;
+        }
+
+        .btn-group {
+            display: flex;
+            gap: 5px;
+        }
+    </style>
+
     <a href="{{ route('sourcing.create') }}" class="btn btn-primary mb-2">Nieuw Product Toevoegen</a>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Naam</th>
-                <th>Beschrijving</th>
-                <th>Afbeelding</th>
-                <th>Prijs</th>
-                <th>Product Categorie</th>
-                <th>Acties</th> <!-- Add a new column for actions -->
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($products as $product)
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
                 <tr>
-                    <td>{{ $product->name }}</td>
-                    <td>{{ $product->description }}</td>
-                    <td>
-                        @if ($product->image_path)
-                            <img style="width: 100px" src="{{ asset('storage/' . $product->image_path) }}" alt="{{ $product->name }} afbeelding">
-                        @else
-                            <span>Geen afbeelding beschikbaar</span>
-                        @endif
-                    </td>
-                    <td>{{ $product->price }}</td>
-                    <td>{{ $product->product_category_id }}</td>
-                    <td>
-                        <!-- Add buttons for edit and delete actions -->
-                        <a href="{{ route('sourcing.edit', $product->id) }}" class="btn btn-warning btn-sm">Bewerken</a>
-
-                        <!-- Verwijder knop (gebruik een formulier om de DELETE-methode te ondersteunen) -->
-                        <form action="{{ route('sourcing.show', $product->id) }}" method="POST" class="inline">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger btn-sm">Verwijderen</button>
-                        </form>
-                    </td>
+                    <th>Naam</th>
+                    <th>Beschrijving</th>
+                    <th>Afbeelding</th>
+                    <th>Prijs</th>
+                    <th>Product Categorie</th>
+                    <th>Acties</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($products as $product)
+                    <tr>
+                        <td>{{ $product->name }}</td>
+                        <td>{{ $product->description }}</td>
+                        <td>
+                            @if ($product->image_path)
+                                <img style="max-width: 100px; max-height: 100px;" src="{{ asset('storage/' . $product->image_path) }}" alt="{{ $product->name }} afbeelding">
+                            @else
+                                <span>Geen afbeelding beschikbaar</span>
+                            @endif
+                        </td>
+                        <td>{{ $product->price }}</td>
+                        <td>{{ $product->product_category_id }}</td>
+                        <td>
+                            <!-- Add buttons for edit and delete actions -->
+                            <a href="{{ route('sourcing.edit', $product->id) }}" class="btn btn-warning btn-sm">Bewerken</a>
+
+                            <!-- Verwijder knop (gebruik een formulier om de DELETE-methode te ondersteunen) -->
+                            <form action="{{ route('sourcing.show', $product->id) }}" method="POST" class="inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger btn-sm">Verwijderen</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </x-app-layout>
