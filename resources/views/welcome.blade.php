17f02e4530eda1132f99ba21d85561cd0f393926
JorisdeVos <jorisdevos0@gmail.com>
2023-11-28 00:11:25 +0100

    First design landing page
    This first design includes the products and main text at the top

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,52 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <h1 class="text-3xl font-bold mb-4">Ontdek de smaak van vernieuwing</h1>
-                    <p class="text-lg">Bij ons draait alles om jouw koffie-ervaring. Proef de perfectie en geniet van onze exclusieve selectie. Of je nu een koffieliefhebber bent of een bedrijfseigenaar die op zoek is naar hoogwaardige koffie, wij hebben wat je zoekt. Bekijk onze selectie hieronder.</p>
+    <div class="relative">
+        <!-- Large Image Spanning Left to Right -->
+        <img src="{{ asset('images/coffee-background.png') }}" alt="Large Image" class="w-full h-72 md:h-96 lg:h-120 object-cover">
+
+        <!-- Text Overlay -->
+        <div class="absolute inset-0 flex flex-col justify-center items-center text-white">
+            <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold mb-4 text-center">Ontdek de smaak van vernieuwing</h1>
+            <p class="text-lg md:text-xl lg:text-2xl text-center">Bij ons draait alles om jouw koffie-ervaring. Proef de perfectie en geniet van onze exclusieve selectie. Of je nu een koffieliefhebber bent of een bedrijfseigenaar die op zoek is naar hoogwaardige koffie, wij hebben wat je zoekt. Bekijk onze selectie hieronder.</p>
+        </div>
+    </div>
+
+    <div class="container mx-auto p-4">
+        <section class="main-content">
+            <div class="flex flex-wrap justify-center gap-4">
+                <div class="card-item md:w-1/3 sm:w-1/2">
+                    <a href="#" class="block bg-white border rounded-md overflow-hidden transition-transform duration-300 transform hover:scale-105 hover:shadow-md group">
+                        <div class="machine-item__brand-image-holder">
+                            <div class="machine-item__brand-image">
+                                <img src="{{ asset('images/products/machine-bit-deluxe.png') }}" alt="Product Image" class="w-full h-auto">
+                            </div>
+                        </div>
+                        <div class="machine-item__detail-holder p-4">
+                            <div class="machine-item__detail">
+                                <div class="machine-item__title text-xl font-bold mb-2">Machine Bit Deluxe</div>
+                                <div class="machine-item__description text-sm">Aantal groepen: 2-3</div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="card-item md:w-1/3 sm:w-1/2">
+                    <a href="#" class="block bg-white border rounded-md overflow-hidden transition-transform duration-300 transform hover:scale-105 hover:shadow-md group">
+                        <div class="machine-item__brand-image-holder">
+                            <div class="machine-item__brand-image">
+                                <img src="{{ asset('images/products/machine-bit-light.png') }}" alt="Product Image" class="w-full h-auto">
+                            </div>
+                        </div>
+                        <div class="machine-item__detail-holder p-4">
+                            <div class="machine-item__detail">
+                                <div class="machine-item__title text-xl font-bold mb-2">Machine Bit Light</div>
+                                <div class="machine-item__description text-sm">Aantal groepen: 2-3</div>
+                            </div>
+                        </div>
+                    </a>
                 </div>
             </div>
-        </div>
+        </section>
     </div>
 </x-app-layout>
