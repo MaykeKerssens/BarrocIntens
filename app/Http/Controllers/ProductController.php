68cd5db5bc50972234ac137df8b0139b90a2c42d
Joost <93526867+Joost56754@users.noreply.github.com>
2023-12-10 21:16:29 +0100

    destroy aangepast en update aangepast

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -30,7 +30,7 @@ class ProductController extends Controller
             'productCategories' => $productCategories,
         ]);
     }
-  /**
+    /**
      * Show the form for editing the specified resource.
      */
     public function edit($id)
@@ -55,25 +55,25 @@ class ProductController extends Controller
             'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048|dimensions:min_width=100,min_height=100',
             'product_category_id' => 'required|exists:product_categories,id',
         ]);
-    
+
         $product = new Product;
         $product->name = $request->name;
         $product->description = $request->description;
         $product->price = $request->price;
         $product->product_category_id = $request->product_category_id;
-    
+
         if ($request->hasFile('image')) {
             $image = $request->file('image');
-            $imageName = time().'.'.$image->getClientOriginalExtension();
+            $imageName = time() . '.' . $image->getClientOriginalExtension();
             $image->storeAs('public/images', $imageName);
             $product->image_path = 'images/' . $imageName;
         }
-    
+
         $product->save();
-    
+
         return redirect()->route('sourcing.index')->with('message', 'Product is aangemaakt');
     }
-    
+
     public function update(Request $request, $id)
     {
         $request->validate([
@@ -83,26 +83,32 @@ class ProductController extends Controller
             'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048|dimensions:min_width=100,min_height=100',
             'product_category_id' => 'required|exists:product_categories,id',
         ]);
-    
+
         $product = Product::findOrFail($id);
-    
+
         $product->name = $request->name;
         $product->description = $request->description;
         $product->price = $request->price;
         $product->product_category_id = $request->product_category_id;
-    
+
         if ($request->hasFile('image')) {
+            $oldImagePath = public_path($product->image_path);
+
+            if (file_exists($oldImagePath)) {
+                unlink($oldImagePath);
+            }
+
             $image = $request->file('image');
-            $imageName = time().'.'.$image->getClientOriginalExtension();
+            $imageName = time() . '.' . $image->getClientOriginalExtension();
             $image->storeAs('public/images', $imageName);
             $product->image_path = 'images/' . $imageName;
         }
-    
+
         $product->save();
-    
+
         return redirect()->route('sourcing.index')->with('message', 'Product is bewerkt');
     }
-    
+
 
     /**
      * Remove the specified resource from storage.
@@ -110,13 +116,13 @@ class ProductController extends Controller
     public function destroy($id)
     {
         $product = Product::findOrFail($id);
-    
-        if (!empty($product->image_path) && Storage::exists($product->image_path)) {
-            Storage::delete($product->image_path);
+
+        if (!empty($product->image_path) && Storage::exists('public/' . $product->image_path)) {
+            Storage::delete('public/' . $product->image_path);
         }
-    
+
         $product->delete();
-    
+
         return redirect()->route('sourcing.index')->with('message', 'Product is verwijderd');
     }
 }
