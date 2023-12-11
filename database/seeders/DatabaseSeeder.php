4fadf539c15b275b1f99ff93ae2f0121b0b2873e
Joost <93526867+Joost56754@users.noreply.github.com>
2023-12-11 16:20:51 +0100

    RoleSeeder toegevoegt in de DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Http\Controllers\InvoicesController;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            RoleSeeder::class,
             UserSeeder::class,
             Companyseeder::class,
             ContractSeeder::class,
