4d35a9b8f77613d03c4c912312d92899c7a71435
MaykeKerssens <93595867+MaykeKerssens@users.noreply.github.com>
2024-01-14 23:19:57 +0100

    Added list of emergency repair requests

--- resources/views/maintenance/requests.blade.php
+++ resources/views/maintenance/requests.blade.php
@@ -9,7 +9,28 @@
         <div class="max-w-7xl mx-auto px-4 py-5 bg-white shadow overflow-hidden flex gap-2">
             <div id='calendar' class="w-3/4"></div>
             <div class="bg-gray-100 p-2 w-1/4">
-                <h4 class="font-semibold text-xl text-gray-800 underline decoration-yellow decoration-2 underline-offset-8 pb-2">Reperaties vandaag:</h4>
+                <h4 class="font-semibold text-xl text-gray-800 underline decoration-yellow decoration-2 underline-offset-8 pb-2">Noodgevallen:</h4>
+                @php
+                    $emergencyRepairRequests = $repairRequests->where('status.name', 'Noodgeval');
+                @endphp
+                @if ($emergencyRepairRequests)
+                    <ul class="list-disc pl-4">
+                        @foreach ($emergencyRepairRequests as $repairRequest)
+                        <li class="mt-4">
+                            <p><b>{{ $repairRequest->company->name }}</b></p>
+                            {{-- <x-table.td>{{ $repairRequest->product->name }}</x-table.td> --}}
+                            <p class="text-gray-500 text-sm">{{ $repairRequest->description }}</p>
+                            <x-secondary-button class="mt-2">
+                                <a href="">Inplannen</a>
+                            </x-secondary-button>
+
+                        </li>
+                    @endforeach
+                    </ul>
+
+                @else
+                    <p>Momenteel geen noodgevallen</p>
+                @endif
 
             </div>
         </div>
@@ -34,7 +55,7 @@
                     <tr>
                         <x-table.td>{{ $repairRequest->company->name }}</x-table.td>
                         <x-table.td>{{ $repairRequest->product->name }}</x-table.td>
-                        <x-table.td>{{ $repairRequest->note }}</x-table.td>
+                        <x-table.td>{{ $repairRequest->description }}</x-table.td>
 
                         <!-- Determine what color the status should have -->
                         <x-table.td
